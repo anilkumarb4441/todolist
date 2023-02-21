1bcaa13339503ae32ab4934ec1bc9a704ef1236d
anilkumarb4441 <anilkumarb4441@gmail.com>
2023-02-21 23:56:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import deleteIcon from './assets/delete.png'
 
 function App() {
+
+const [inputState, setInputField] = useState('');
+const [todoData, setTodoData] = useState(['react']);
+
+
+const onSubmitBtn = (e)=>{
+  if(e.keyCode === 13) { 
+  e.preventDefault();
+    setTodoData([...todoData, inputState]);
+  setInputField('');
+}
+  
+}
+
+const deleteTodod = (etm)=>{
+const deleteItem = todoData.filter((item)=>item !==etm);
+setTodoData(deleteItem);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='totoContainer'>
+     <form>
+        <input  type='text' placeholder='' value={inputState} onChange={(e)=>setInputField(e.target.value)} onKeyDown={(e)=>onSubmitBtn(e)}/>
+     </form>
+     <div className='todoListWraper'>
+      { todoData.length > 0 ? todoData.map((item, idx)=>{
+        return(
+        <div className='mainContainer' key={idx}>
+        <input type='checkbox'/>
+        <p className='todoName'>{item}</p>
+        <img src={deleteIcon} alt='deletebtn' onClick={()=>deleteTodod(item)}/>
+     </div>
+      );
+      })
+        
+     :null
+      }
+     </div>
+     </div>
     </div>
   );
 }
